df1258af2358046a247c6377ea647db1b63237fd
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-09-06 06:32:20 -0700

    Projectile shooting and applying damage

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float lifeTime;
+    [SerializeField] private int damage;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destructable destructable = collision.transform.root.GetComponent<Destructable>();
+
+        if(destructable != null)
+        {
+            destructable.ApplyDamage(damage);
+        }
+
+        Destroy(gameObject);    
+    }
+}
